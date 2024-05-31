ad25e8657f8aa2d605ba665bc2d8bfde3c2e690c
flcat <flcat.xd@gmail.com>
2024-05-31 17:53:04 +0900

    init

---
+++ src/main/java/com/flcat/Dospital/DospitalApplication.java
@@ -0,0 +1,13 @@
+package com.flcat.Dospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DospitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flcat/Dospital/DospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flcat.Dospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
